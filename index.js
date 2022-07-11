e0d1505e4c2138a4f2cf762b9239025e44aef37a
David Stockdale <dstock3@gmail.com>
2022-07-11 17:09:47 -0500

    Installed mongoose, added mongoDB conntection string, added env file.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+//imports
 const express = require("express");
+const mongoose = require('mongoose');
+
+//connect to DB
+mongoose.connect(
+    process.env.URI_STRING,
+    { useNewUrlParser: true },
+    ()=> console.log("connected to db"));
+
+//instantiate express
 const app = express();
 
 //import routes
-
 const authRoute = require('./routes/auth');
 
 ///Route middlewares
 
 //Everything in the auth route is going to have the prefix '/api/user'
 app.use('/api/user', authRoute);
-
 
 app.listen(3003, () => console.log("Server is running"))
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,11 @@ const router = require('express').Router();
 router.post('/register', (req, res) => {
     res.send("register page")
 
-})
+});
 
+/*
+router.post('login', (req, res) => {
+
+});
+*/
 module.exports = router;
