81bbd236e51e6eb67a5fd71e0d0fa3494cb2dfbc
David Stockdale <dstock3@gmail.com>
2022-07-13 17:16:16 -0500

    Added post route for register.

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 //imports
 const express = require("express");
 const mongoose = require('mongoose');
-const dotenv = require('dotenv')
-
+const dotenv = require('dotenv');
 dotenv.config();
+
+//import routes
+const authRoute = require('./routes/auth');
+
+//instantiate express
+const app = express();
 
 //connect to DB
 mongoose.connect(
     process.env.URI_STRING,
     { useNewUrlParser: true },
-    ()=> console.log("connected to db"));
+    ()=> console.log("connected to db")
+);
 
-//instantiate express
-const app = express();
-
-//import routes
-const authRoute = require('./routes/auth');
+//Middleware
+app.use(express.json());
 
 ///Route middlewares
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 //Authentication routes
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/register', (req, res) => {
-    res.send("register page")
-
+router.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const savedUser = await user.save()
+        res.send(savedUser)
+    } catch(err) {
+        res.status(400).send(err)
+    }
 });
 
 /*
