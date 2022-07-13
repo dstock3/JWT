520c1038fa9c5831e4f80370ebc9419f8b6f1bc9
David Stockdale <dstock3@gmail.com>
2022-07-13 17:47:16 -0500

    Created separate validation file in root. Added code to check if email already exists in register post.

---
+++ validation.js
@@ -0,0 +1,25 @@
+//Validation
+const Joi = require('@hapi/joi');
+
+//Register validation
+const registerValidation = body => {
+    const schema = {
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    };
+    return Joi.validate(body, schema);
+}
+
+//login Validation
+const loginValidation = body => {
+    const schema = {
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    };
+    return Joi.validate(body, schema);
+}
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
