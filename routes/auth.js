d4a820b61115a45fe4a851e10a27552e8bee4463
David Stockdale <dstock3@gmail.com>
2022-07-13 17:32:34 -0500

    Installed joi validation package and employed in register post route.

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,21 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+//Validation
+const Joi = require('@hapi/joi');
+
+const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+};
+
 router.post('/register', async (req, res) => {
+    //Validate data
+    const validation = Joi.validate(req.body, schema);
+    res.send(validation)
+    /*
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -14,6 +28,7 @@ router.post('/register', async (req, res) => {
     } catch(err) {
         res.status(400).send(err)
     }
+    */
 });
 
 /*
