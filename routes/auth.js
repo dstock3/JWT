aaede5a2849e08636635925078ab16051adfa7b6
David Stockdale <dstock3@gmail.com>
2022-07-15 11:47:07 -0500

    Employed bcrypt to add salt and hash passwords.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const { registerValidation }= require('../validation')
+const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req, res) => {
     //Validate data
@@ -12,10 +13,15 @@ router.post('/register', async (req, res) => {
     const emailExists = await User.findOne({ email: req.body.email })
     if (emailExists) return res.status(400).send('Email already exists.');
 
+    //Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Create new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
